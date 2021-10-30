31ca5b3278cf9407d09fb8a2a3789cb4e867de13
sbx_user1051 <sbx_user1051@169.254.249.213>
2021-10-30 17:50:26 +0000

    Sat Oct 30 17:50:26 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,31 +8,23 @@ def checkout(skus):
     total = 0
     occrs = {}
     offers = {"A" : [3, 20], "B" : [2, 15]}
+    costs = {"A" : 50, "B" : 30, "C" : 20, "D" : 15}
 
     for sku in skus:
 
-        cost = int(line[1])
-        item = line[0]
+        cost = costs[sku]
 
         total += cost   #Add cost to total
-        curr = occrs.get(item, 0)  #Current no. of occurences
-        occrs[item] = curr + 1 #Increment no. of occurences
+        curr = occrs.get(sku, 0)  #Current no. of occurences
+        occrs[sku] = curr + 1 #Increment no. of occurences
 
-        offer = line[2].strip().split(" ")
-
-        #Check offer exists for item
-        if len(offer) > 2:
-
-            if item in offers:
-                #Check if offer satisified
-                if occrs[item] == offers[item][0]:
-                    total -= offers[item][1]
-            else:
-                #Add offer
-                num_needed = int(offer[0][:-1])
-                discount = cost * num_needed - int(offer[2])
-                offers[item] = [num_needed, discount]
+        if sku in offers:
+            #Check if offer satisified
+            if occrs[sku] == offers[sku][0]:
+                total -= offers[sku][1]
 
     return total
+
+print(checkout(test_input))
 
 
