ee862a447b3e81acc4fb5c9488ae80e3a239e165
sbx_user1051 <sbx_user1051@169.254.249.213>
2021-10-30 17:16:16 +0000

    Sat Oct 30 17:16:16 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,16 @@
 
+
+test_input = """+------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
+| A    | 50    | 3A for 130     |
+| A    | 50    | 3A for 130     |
++------+-------+----------------+"""
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -7,10 +19,12 @@ def checkout(skus):
     #   return -1
 
     #Split input into lines
-    #   remove first and last
+    #   remove first 3 and last
     lines = skus.splitlines()[3:-1]
 
     #Split each line by "|"
+    lines = [item.split("|")[1:-1] for item in lines]
+    print(lines)
 
 
     #Loop sku
@@ -20,11 +34,10 @@ def checkout(skus):
     #       if so, satisifed?
     #           if so, reduce sum
 
-
-
-
-
-
     return -1
+
+
+checkout(test_input)
+
 
 
